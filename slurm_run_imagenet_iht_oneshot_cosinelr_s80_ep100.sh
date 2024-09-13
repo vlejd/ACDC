00afcd1b33c706e76b5193fd4f08f75e2e6a2f10
vlejd <vladimir.v.macko@gmail.com>
2024-09-13 15:18:23 +0200

    Try scratch

--- slurm_run_imagenet_iht_oneshot_cosinelr_s80_ep100.sh
+++ slurm_run_imagenet_iht_oneshot_cosinelr_s80_ep100.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 #SBATCH -J "imgnet"     # name of job in SLURM
 #SBATCH -p gpu
-#SBATCH -G 2
+#SBATCH -G 1
 #SBATCH -c 20
 #SBATCH -o joblog/slurm_run_imagenet_iht_oneshot_cosinelr_s80_ep100.txt
 #SBATCH -e joblog/slurm_run_imagenet_iht_oneshot_cosinelr_s80_ep100.txt
 #SBATCH --account=p490-24-t
 #SBATCH --time=60
 #SBATCH --mem=200GB
-#or srun  -J "imgnet" -c 40 -p gpu -G 2 -o joblog/slurm_run_imagenet_iht_oneshot_cosinelr_s80_ep100.txt -e joblog/slurm_run_imagenet_iht_oneshot_cosinelr_s80_ep100.txt --account=p490-24-t --time=60 --mem=200GB slurm_run_imagenet_iht_oneshot_cosinelr_s80_ep100.sh
+#--exclusive probably gets me 4 gpus...
+#or srun  -J "imgnet" -c 40 -p gpu -G 1 -o joblog/slurm_run_imagenet_iht_oneshot_cosinelr_s80_ep100.txt -e joblog/slurm_run_imagenet_iht_oneshot_cosinelr_s80_ep100.txt --account=p490-24-t --time=2800 --mem=200GB slurm_run_imagenet_iht_oneshot_cosinelr_s80_ep100.sh
 
 echo "Launched at $(date)"
 echo "Job ID: ${SLURM_JOBID}"
@@ -20,10 +21,25 @@ echo $SHELL
 echo "Loading cuda"
 module load cuda/12.0.1
 
+echo "Move the dataset to /work"
+
+target_dataset=/scratch/p490-24-t/data/imagenet
+mkdir -p ${target_dataset}
+
+echo "Copy train imagenet to ${target_dataset}"
+date
+rsync -r /projects/p490-24-t/data/imagenet/train ${target_dataset}
+date
+echo "Copy val imagenet to ${target_dataset}"
+date
+rsync -r /projects/p490-24-t/data/imagenet/val ${target_dataset}
+date
+
+
 echo "Lets get this party started!"
 
 
-declare -a gpu=0,1 #0,1,2,3
+declare -a gpu=0 #0,1,2,3
 declare -a manual_seed=(3)
 
 
@@ -31,7 +47,7 @@ for ((j=0;j<${#manual_seed[@]};++j));
 do
 python main.py \
 	--dset=imagenet \
-	--dset_path=/projects/p490-24-t/data/imagenet/ \
+	--dset_path=${target_dataset} \
 	--arch=resnet50 \
 	--config_path=./configs/neurips/iht_imagenet_resnet50_insta_cosinelr_s80_ep100.yaml \
 	--workers=40 \
