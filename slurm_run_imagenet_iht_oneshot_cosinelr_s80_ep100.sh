dfc66063f30bb3fcaaa569ac2c5745736fec7bfb
vlejd <vladimir.v.macko@gmail.com>
2024-09-13 13:59:35 +0200

    Multiple gpus
    speed 0.4, not much improvement

--- slurm_run_imagenet_iht_oneshot_cosinelr_s80_ep100.sh
+++ slurm_run_imagenet_iht_oneshot_cosinelr_s80_ep100.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 #SBATCH -J "imgnet"     # name of job in SLURM
 #SBATCH -p gpu
-#SBATCH -G 1
+#SBATCH -G 2
 #SBATCH -c 20
 #SBATCH -o joblog/slurm_run_imagenet_iht_oneshot_cosinelr_s80_ep100.txt
 #SBATCH -e joblog/slurm_run_imagenet_iht_oneshot_cosinelr_s80_ep100.txt
 #SBATCH --account=p490-24-t
-#SBATCH --time=120
+#SBATCH --time=60
 #SBATCH --mem=200GB
-#or srun  -J "imgnet" -c 20 -p gpu -G 1 -o joblog/slurm_run_imagenet_iht_oneshot_cosinelr_s80_ep100.txt -e joblog/slurm_run_imagenet_iht_oneshot_cosinelr_s80_ep100.txt --account=p490-24-t --time=60 --mem=200GB slurm_run_imagenet_iht_oneshot_cosinelr_s80_ep100.sh
+#or srun  -J "imgnet" -c 40 -p gpu -G 2 -o joblog/slurm_run_imagenet_iht_oneshot_cosinelr_s80_ep100.txt -e joblog/slurm_run_imagenet_iht_oneshot_cosinelr_s80_ep100.txt --account=p490-24-t --time=60 --mem=200GB slurm_run_imagenet_iht_oneshot_cosinelr_s80_ep100.sh
 
 echo "Launched at $(date)"
 echo "Job ID: ${SLURM_JOBID}"
@@ -20,25 +20,10 @@ echo $SHELL
 echo "Loading cuda"
 module load cuda/12.0.1
 
-echo "Move the dataset to /work"
-
-target_dataset=/work/${SLURM_JOBID}/imagenet
-mkdir ${target_dataset}
-
-echo "Copy train imagenet to ${target_dataset}"
-date
-cp -r /projects/p490-24-t/data/imagenet/train ${target_dataset}
-date
-echo "Copy val imagenet to ${target_dataset}"
-date
-cp -r /projects/p490-24-t/data/imagenet/val ${target_dataset}
-date
-
-
 echo "Lets get this party started!"
 
 
-declare -a gpu=0 #0,1,2,3
+declare -a gpu=0,1 #0,1,2,3
 declare -a manual_seed=(3)
 
 
@@ -46,10 +31,10 @@ for ((j=0;j<${#manual_seed[@]};++j));
 do
 python main.py \
 	--dset=imagenet \
-	--dset_path=${target_dataset} \
+	--dset_path=/projects/p490-24-t/data/imagenet/ \
 	--arch=resnet50 \
 	--config_path=./configs/neurips/iht_imagenet_resnet50_insta_cosinelr_s80_ep100.yaml \
-	--workers=20 \
+	--workers=40 \
 	--epochs=100 \
 	--fp16 \
 	--reset_momentum_after_recycling \
